da41bd19230520ddd2d74f91ddefddc39ab0ef06
Thirupathi Maram <thirupathi_maram@persistent.co.in>
2016-02-12 15:10:52 +0530

    Added new Address.java file

---
+++ hib-app/src/main/java/com/psl/model/Address.java
@@ -0,0 +1,77 @@
+/**
+ *
+ */
+package com.psl.model;
+
+/**
+ * @author thirupathi_maram
+ *
+ */
+public class Address {
+
+	private Long id;
+	private String street;
+	private String city;
+	private String zipcode;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street
+	 *            the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city
+	 *            the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the zipcode
+	 */
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	/**
+	 * @param zipcode
+	 *            the zipcode to set
+	 */
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+}
